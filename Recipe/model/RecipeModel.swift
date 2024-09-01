e17a149e6ecc344a4f1d68773ad55bbf152b8798
uwi0 <kakapoweb@gmail.com>
2024-09-01 08:54:56 +0700

    created rcipe model

---
+++ Recipe/model/RecipeModel.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+struct RecipeModel: Identifiable {
+    let id = UUID()
+    let name: String
+    let caloriesPer100Grams: Int
+    let recipe: String
+    let recipeImage: String
+    let recipeUrl: String
+}
---
+++ Recipe/views/RecipeView.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+struct RecipeView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    RecipeView()
+}
