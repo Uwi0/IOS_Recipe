788d69dd31dd402fd27b3d874e67c18eb0ae089a
uwi0 <kakapoweb@gmail.com>
2024-09-01 08:50:05 +0700

    Initial Commit

---
+++ Recipe/RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp.swift
+//  Recipe
+//
+//  Created by dwi prasetyo on 01/09/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
